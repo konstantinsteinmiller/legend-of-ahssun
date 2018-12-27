bc48506b8754035a1d4fa92e38d8efaa1511420d
Konstantin <steinmille@gmail.com>
2018-12-27 20:29:23 +0100

    added cube for videos and sounds

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-function goToLink (event, url) {
-  var type = event.type;
-
-  if (
-    (type === 'click') ||
-    (type === 'keydown' && event.keyCode === 13)
-  ) {
-    window.location.href = url;
-
-    // event.preventDefault();
-    event.stopPropagation();
-  }
-}
-
 function closeNavigation(url) {
   var toggle = document.querySelector('#navi-toggle')
   toggle.checked = false
