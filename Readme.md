b09375167cf94355859aa0ea0103e2d9680849b5
Konstantin <steinmille@gmail.com>
2018-12-28 17:06:34 +0100

    added language picker and features translations

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 This is the code for the official Legend of Ahssun website.
 
 Todo:
+- fill features section
 - credits section
 - download instruction onto cards
-- fill features section
 - translation and language-picker
--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-function closeNavigation(url) {
-  var toggle = document.querySelector('#navi-toggle')
+function closeNavigation() {
+  let toggle = document.querySelector('#navi-toggle');
   toggle.checked = false
+}
+
+function setLanguage(locale) {
+  const $clearLocales = document.querySelector('.header__locale--selected');
+  $clearLocales && $clearLocales.classList.remove('header__locale--selected');
+  const $locale = document.querySelector('.header__locale--' + locale);
+  $locale.classList.add('header__locale--selected')
+
+  const $dataTNodes = document.querySelectorAll('[data-'+locale+']');
+  $dataTNodes.forEach(node => {
+    const translation = node.dataset[locale];
+    node.innerHTML = translation;
+  });
+}
+
+function onLoad() {
+  const queries = location.search ? location.search.split('=') : [];
+  const lang = queries.filter((q) => q === 'de' || q === 'en').pop();
+
+  if (lang === 'de') {
+    setLanguage('de')
+  } else {
+    setLanguage('en')
+  }
 }
