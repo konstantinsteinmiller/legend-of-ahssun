80967872358353087a09fda23f5fdfc8f5f38cb5
Konstantin <steinmille@gmail.com>
2018-12-30 17:02:52 +0100

    added translation to image gallery and improved handling

--- js/main.js
+++ js/main.js
@@ -4,20 +4,22 @@ function closeNavigation() {
 }
 
 function setLanguage(locale) {
+  const queries = location.search ? location.search.split('=') : [];
+  const newQueries = queries.map(q => {
+    return (q === 'de' || q === 'en')? locale : q;
+  }).join('=');
+  
   const $clearLocales = document.querySelector('.header__locale--selected');
   $clearLocales && $clearLocales.classList.remove('header__locale--selected');
   const $locale = document.querySelector('.header__locale--' + locale);
-  $locale.classList.add('header__locale--selected')
+  $locale && $locale.classList.add('header__locale--selected')
 
   const $dataTNodes = document.querySelectorAll('[data-'+locale+']');
   $dataTNodes.forEach(node => {
     const translation = node.dataset[locale];
     node.innerHTML = translation;
   });
-  const queries = location.search ? location.search.split('=') : [];
-  const newQueries = queries.map(q => {
-    return (q === 'de' || q === 'en')? locale : q;
-  }).join('=');
+
   if (newQueries !== location.search) location.search = newQueries;
   localStorage.lang = locale;
 }
@@ -26,9 +28,7 @@ function onLoad() {
   const queries = location.search ? location.search.split('=') : [];
   const lang = queries.filter(q => q === 'de' || q === 'en').pop();
   const storedLocale = lang ? lang : localStorage.lang
-  if (lang === 'en') {
-    setLanguage('en')
-  } else if (storedLocale){
+  if (storedLocale){
     setLanguage(storedLocale)
   } else {
     setLanguage('de')
